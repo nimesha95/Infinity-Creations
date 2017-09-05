807ca4611f7452302855338d6f6a79f0432d592c
nimesha95 <nimesha95@live.com>
2017-09-06 01:37:47 +0530

    Addeed nav bar and some basic views

---
+++ resources/views/shop/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+
+@section('title')
+    Shopping Cart
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src="https://ctl.s6img.com/society6/img/WYLJtDHQOiaM-KHfS89VR-_hFfM/h_550,w_550/coffee-mugs/small/right/greybg/~artwork/s6-0024/a/9706823_10429294/~~/coffee-talk-8w4-mugs.jpg" alt="...">
+                <div class="caption">
+                    <h3>Thumbnail label</h3>
+                    <p>...</p>
+                    <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
